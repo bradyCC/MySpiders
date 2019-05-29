c775200217671f45a74608505f0d03f007397fd5
Brady <acheng_69@163.com>
2019-05-29 23:03:40 +0800

    全职高手小说爬虫

--- novel/novel/spiders/quanzhigaoshou.py
+++ novel/novel/spiders/quanzhigaoshou.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import json
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from novel.items import NovelItem
@@ -11,7 +10,6 @@ class QuanzhigaoshouSpider(CrawlSpider):
     start_urls = ['https://www.81xzw.com/book/16628/']
 
     def parse_start_url(self, response):
-        print(response.url)
         pass
 
     rules = (
@@ -21,18 +19,15 @@ class QuanzhigaoshouSpider(CrawlSpider):
     def parse_item(self, response):
         print(response.url)
         item = NovelItem()
-        domain = response.url.split('/book')[0]
-        if domain == 'https://81xzw.upanbook.com':
-            item['bookName'] = response.xpath('//title/text()').get().split('_')[1]
-            item['chapterNum'] = response.xpath('//p[@class="title"]/text()').get().split(' ')[0]
-            item['chapterName'] = response.xpath('//p[@class="title"]/text()').get().split(' ')[1]
-            item['chapterUrl'] = response.url
-            item['chapterContent'] = ''.join(response.xpath('//div[@class="chaptercontent"]/text()').extract()).strip()
-            yield item
-        elif domain == 'https://www.81xzw.com':
-            item['bookName'] = response.xpath('//div[@class="novel"]/h1/text()').get().split(' ')[0]
-            item['chapterNum'] = response.xpath('//div[@class="novel"]/h1/text()').get().split(' ')[1]
-            item['chapterName'] = response.xpath('//div[@class="novel"]/h1/text()').get().split(' ')[2]
-            item['chapterUrl'] = response.url
-            item['chapterContent'] = ''.join(response.xpath('//div[@class="yd_text2"]/text()').extract()).strip()
-            yield item
+        title = response.xpath('//div[@class="novel"]/h1/text()').get().split(' ', 2)
+        if len(title) == 3:
+            item['bookName'] = title[0]
+            item['chapterNum'] = title[1]
+            item['chapterName'] = title[2]
+        elif len(title) == 2:
+            item['bookName'] = title[0]
+            item['chapterNum'] = '第一千七百二十九章'
+            item['chapterName'] = title[1]
+        item['chapterUrl'] = response.url
+        item['chapterContent'] = ''.join(response.xpath('//div[@class="yd_text2"]/text()').extract()).strip()
+        yield item
